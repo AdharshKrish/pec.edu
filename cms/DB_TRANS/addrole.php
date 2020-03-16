1a7e62a0e58faf77eacf1142cd2cbee46f9674cf
Purushottam Banerjee <purushottambanerjee@gmail.com>
2020-03-16 22:29:55 +0530

    use new sql

--- cms/DB_TRANS/addrole.php
+++ cms/DB_TRANS/addrole.php
@@ -11,20 +11,26 @@ $query_check_exists="SELECT * FROM login WHERE email='".$email."'";
 $result=mysqli_query($db_con,$query_check_exists);
 if($result)
 {
-    $arr=mysqli_fetch_assoc($result);
+    $arr=mysqli_fetch_assoc($result);$idref=$arr['id'];
     $cv=mysqli_num_rows($result);
     if($cv>0)
     {
-        $query="UPDATE login set desg='".$role."'  where id=".$arr['id'];
+        $query="SELECT * FROM `role` WHERE id=$idref AND desg='$role'";
        // echo "UPDATE login set desg='".$role."'  where id=".$arr['id'];
-        if(mysqli_query($db_con,$query))
-        {
-            header("Location: ../pages/webmasteraddrole.php?message=successfully added role");
+       $result=mysqli_query($db_con,$query);
+
+       $cv=mysqli_num_rows($result);
+
+       if($cv>0)
+       {
+           echo $query;
+        // header("Location: ../pages/webmasteraddrole.php?message=already exists");
             exit();
         }
         else {
-            echo 1;
-           // header("Location: ../pec/error/error404.html");
+            $query="INSERT INTO `role`(`idref`, `desg`) VALUES ($idref,'$role')";
+            mysqli_query($db_con,$query);    
+            header("Location: ../pages/webmasteraddrole.php?message=successfully added role");
             exit();
         }
     }
