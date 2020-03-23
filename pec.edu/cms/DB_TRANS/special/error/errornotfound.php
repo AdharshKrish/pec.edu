e26ff0e0efe5e6f7317e47a50712c62bcd8c1a85
RChirom <rupamchirom@yahoo.com>
2020-03-23 10:30:25 +0530

    deleted extra pec.edu

--- pec.edu/cms/DB_TRANS/special/error/errornotfound.php
+++
@@ -1,4 +0,0 @@
-<?php
-$msg_id=$_GET['msg_id'];
-echo $msg_id;
-?>
--- pec.edu/cms/JS/pages/signout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-//$role=$_SESSION['role'];
-
-session_destroy();
-header("Location: ../index.php");
-exit();
-?>
