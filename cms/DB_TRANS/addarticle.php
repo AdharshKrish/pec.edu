e0eba62f595f38ca942869927533e7ec3176d203
Purushottam Banerjee <purushottambanerjee@gmail.com>
2020-03-25 12:05:58 +0530

    Add files via upload

--- cms/DB_TRANS/addarticle.php
+++ cms/DB_TRANS/addarticle.php
@@ -6,19 +6,20 @@ $dep=$_POST['dept'];
 $img = $_FILES['file']['name'];$img = time().$img;
 $author=$_POST['author'];
 $query="INSERT INTO `article`(`name`, `email`, `dep`, `img`,`author`) VALUES ( '".$name."','".$email."','".$dep."','".$img."','".$author."')";
+
 if(mysqli_query($db_con,$query))
 { 
 $filename = $img;
 $file_type=$_FILES['file']['type'];
 //$filename=substr($filename,3);
 move_uploaded_file($_FILES['file']['tmp_name'],'../articles/'.$filename);
-header("Location: ../../".$dep."/webhandler_viewmemo?message=succesfully entered new post !!");
+header("Location: ../pages/webhandler_viewmemo.php?message=succesfully entered new post !!");
 exit();
 
 
 }
 else{
-    header("Location: ../../pages/webhandler_viewmemo.php?message=error has occured");
+    header("Location: ../pages/webhandler_viewmemo.php?message=error has occured");
     exit();
 }
 ?>
