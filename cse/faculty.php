e7760e55ba1804cdfe94c9b038836945569fa5fa
RChirom <rupamchirom@yahoo.com>
2020-03-26 17:24:13 +0530

    added staffs

--- cse/faculty.php
+++ cse/faculty.php
@@ -196,6 +196,7 @@
 
         .label {
             padding-left: unset;
+            margin-right: 5px;
             font-size: 14px;
             font-weight: bold;
             color: #707070;
@@ -225,7 +226,7 @@
 
         .degree {
             text-align: center;
-            margin-top:10px;
+            margin-top: 10px;
             font-size: 18px;
         }
 
@@ -663,6 +664,42 @@
                         </div>
                         
                     </div></a>';
+                    }
+                } ?>
+
+<?php
+                $query = "SELECT * FROM staff where dep='cse'";
+
+                if ($result = mysqli_query($db_con, $query)) {
+
+
+                    while ($arr = mysqli_fetch_assoc($result)) {
+                        $name = $arr['name'];
+                        $id = $arr['id'];
+                        $role = $arr['role'];
+                        $number = $arr['phone'];
+  
+                        if (mysqli_num_rows($result) > 0)
+                        $file_path = $arr['picname'];
+                        else
+                        $file_path = "profile-placeholder.png";
+
+
+                        echo '<div class="col-sm-6 col-md-6 col-lg-3 card-parent"><a href="facultypage.php?email=' . $email . '">
+                                <div class="card">
+                                    <p class="designation" style="background-color:#33DD88">Staff</p>
+                                    <h3 class="faculty-name">' . $name . '</h3>
+                                    <div class="thumbnail">
+                                        <img src="../cms/profilepic/' . $file_path . '" alt="faculty-image" style="object-fit: cover;
+                                        width: 200px;
+                                        height: 200px;">
+                                    </div>
+                                    <p class="degree">' . $role . '</p>
+                                    <div class="details">
+                                        <label class="label"> Phone Number </label>' . $number . '<br>
+                                    </div>
+                                </div>   
+                            </div></a>';
                     }
                 } ?>
 
