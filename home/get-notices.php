b76ceed3ec505428bcfb708030fcbcb46e7c5f14
Adharsh <adharshkrish@outlook.com>
2020-03-27 10:48:29 +0530

    bad card style

--- home/get-notices.php
+++ home/get-notices.php
@@ -26,49 +26,38 @@ if($result=mysqli_query($db_con,$query))
         }
         $cid=$arr['id'];
         $dept=$arr['department'];
-        if($dept=="cse"){
-            $dept="Department of CSE";
-            $clr="#38ada9";
-        }else if($dept=="ce"){
-            $dept="Department of CE";
-            $clr="#a085ea";
-        }else if($dept=="ece"){
-            $dept="Department of ECE";
-            $clr="#eb2f06";
+        if($dept=="all"){
+            $dept="All Department";
+            $clr="#3c6382";
         }else{
             $dept="General";
-            $clr="#41a700";
+            $clr="#0ea2be";
         }
             
 
         echo'<btn class=cad onclick="getFullContent('.$cid.')" data-toggle="modal" data-target="#full-notice">
         <div class=header>
-            <div class=tag style="background-color:'.$clr.'">'.$dept.'</div>
-            <span style="float: right">
-                    
-                   
-            <form method="post" action="../ics/downloadcalendar.php">
-            <input type="hidden" name="date_start" value='.$start.'>
-            <input type="hidden" name="date_end" value='.$end.'>
-            <input type="hidden" name="location" value="Pondicherry Engineering College, Pillaichavady">
-            <input type="hidden" name="description" value='.$summary.'>
+            <div class=tag style="background-color:'.$clr.'">'.$dept.'</div>';
             
-          <input align="right" type="image" value="Add to Calendar"class="cal-icon" src="../img/calendar.svg" height="24" width="24" alt="calender-icon" title="Add to my Calender">  </span></input>
-            </form></span>
-            <div class=row>
-                <div class=col-1>';
-                if($bul && !isset($_COOKIE[$cid]))
-                    echo '<span id="bul1'.$cid.'" class=bul>&bull;</span>';
-                echo '</div><div class=col-10>
-                    <div class=head>';
-                    if($bul && !isset($_COOKIE[$cid]))
-                        echo '<new id="bul2'.$cid.'">New</new><br>';
-                    else
-                        echo '<new style="height:25px;display:block"></new>';
-                echo '<start>'.$start.'</start>
-                        <p>'.$end.'</p>
-                    </div>
-                </div>
+        if($bul && !isset($_COOKIE[$cid]))
+            echo'<div id="bul'.$cid.'"><span class=bul>&bull;</span><span>New</span></div>';
+        else
+            echo'<div style="height:55px;width:20px"></div>';
+        echo'<span>            
+                <form method="post" action="../ics/downloadcalendar.php">
+                    <input type="hidden" name="date_start" value='.$start.'>
+                    <input type="hidden" name="date_end" value='.$end.'>
+                    <input type="hidden" name="location" value="Pondicherry Engineering College, Pillaichavady">
+                    <input type="hidden" name="description" value='.$summary.'>
+                    
+                    <input style="float:right" type="image" value="Add to Calendar"class="cal-icon" src="../img/calendar.svg" height="24" width="24" alt="calender-icon" title="Add to my Calender">  </span></input>
+                </form>
+            </span>
+            <div class=row style="margin-left:10px">
+                <div class=col-3>From</div>
+                <div class=col-8>'.$start.'</div>
+                <div class=col-3>To</div>
+                <div class=col-8>'.$end.'</div>
             </div>
         </div>
         <div class=summary>
@@ -87,8 +76,22 @@ if($result=mysqli_query($db_con,$query))
         }
         echo'   </div>
         </btn>';
-
-
     }
+    //     <div class=row>
+    //     <div class=col-1>';
+    //     if($bul && !isset($_COOKIE[$cid]))
+    //         echo '<span id="bul1'.$cid.'" class=bul>&bull;</span>';
+    //     echo '</div><div class=col-10>
+    //         <div class=head>';
+    //         if($bul && !isset($_COOKIE[$cid]))
+    //             echo '<new id="bul2'.$cid.'">New</new><br>';
+    //         else
+    //             echo '<new style="height:25px;display:block"></new>';
+    //     echo '<start>From'.$start.'</start>
+    //             <p>To'.$end.'</p>
+    //         </div>
+    //     </div>
+    // </div>
+
 }
 ?>
