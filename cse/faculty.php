36cb2aa3165ab5d8ce93ece8a5b39673e38b1efc
Purushottam Banerjee <purushottambanerjee@gmail.com>
2020-03-29 20:07:46 +0530

    Add files via upload

--- cse/faculty.php
+++ cse/faculty.php
@@ -372,13 +372,22 @@
         <!--ENTER MAIN CONTENT HERE-->
 
 
-        <?php
-        include("db_con.php");
+       
+        <div class="faculty-header"> 
+            <h2>Faculty</h2>
+        </div>
+        <div class="container-fluid  card-container">
+
+            <div class="row ">
+
+          <?php 
+          
+          include("db_con.php");
 
         $query = "SELECT l.id,l.email FROM login l INNER JOIN role r ON l.id=r.idref AND l.department='cse' AND r.desg='HOD'";
 
         $result = mysqli_query($db_con, $query);
-        if ($result) {
+        if (mysqli_num_rows($result)) {
            // echo $query;
             $arr = mysqli_fetch_assoc($result);
             $email=$arr['email'];
@@ -400,16 +409,9 @@
             $result = mysqli_query($db_con, $query);
             $arr = mysqli_fetch_assoc($result);
             $file_path = $arr['file_name'];
-        }
-        ?>
-        <div class="faculty-header"> 
-            <h2>Faculty</h2>
-        </div>
-        <div class="container-fluid  card-container">
 
-            <div class="row ">
 
-          <?php  echo '
+            echo '
             <div class="col-sm-4 col-md-6 col-lg-3  card-parent">
             <a href="facultypage.php?email=' . $email . '">
            
@@ -432,7 +434,14 @@
                         </a>
                     </div>
                
-                    ';?>
+                    ';
+
+
+        }
+          
+          
+          
+         ?>
                 <?php
                 $query = "SELECT * FROM basic_faculty_info where post_tier='Professor' and department='cse'";
 
