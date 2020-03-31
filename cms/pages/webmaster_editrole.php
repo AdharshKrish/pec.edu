b1f3faf5b91367ff209d61a8a03c50ae67426cc1
RChirom <rupamchirom@yahoo.com>
2020-03-31 19:24:44 +0530

    reverted reverted commit

--- cms/pages/webmaster_editrole.php
+++ cms/pages/webmaster_editrole.php
@@ -66,9 +66,10 @@ $result = mysqli_query($db_con, $query);
                         <li> <a href="webmasteraddrole.php" id=""> ADD ROLES </a></li>
                         <li> <a href="webmasteraddmemo.php" id=""> ADD MEMO </a></li>
                         <li> <a href="webmaster_viewmemo.php"> VIEW MEMO </a></li>
-                        <li> <a href="webmaster_role.php"> VIEW ROLES </a></li>
-                        <li> <a href="webmaster_viewfaculty.php" style="color: black;"> VIEW FACULTY DATA SET </a></li>
+                        <li> <a href="webmaster_role.php"  style="color: black;"> VIEW ROLES </a></li>
+                        <li> <a href="webmaster_viewfaculty.php"> FACULTY ACCESS LIST </a></li>
                         <li><a href="signout.php" style="color: #2E8690">SIGNOUT</a></li>
+                        <li><a href="../../files/cms-helpbook.docx" target="blank" style="color: #0000EE">HELPBOOK</a></li>
                     </ul>
                 </div>
                 <a href='./webmaster_role.php'><button class="btn">
@@ -78,7 +79,7 @@ $result = mysqli_query($db_con, $query);
                     <div class="panel panel-default"> <?php
                                                         if ($error == 1)
                                                             echo "  <div class='alert alert-success'>
-                 <strong>SUCCESSFULL</strong> " . $message . "
+                 <strong>STATUS: </strong> " . $message . "
                </div>";
                                                         ?>
                         <div class="panel-body">
