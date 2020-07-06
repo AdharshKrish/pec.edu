fc66c923c1a693bd4f74a1454b2888b75e5aafc9
RChirom <rupamchirom@yahoo.com>
2020-07-06 14:17:21 +0530

    delete faculty bug fixed

--- cms/DB_TRANS/delete_faculty.php
+++ cms/DB_TRANS/delete_faculty.php
@@ -3,7 +3,8 @@ $cntid=$_GET['id'];
 require_once('../DB_TRANS/db_con.php');
 $query="DELETE  FROM `login` where id=".$cntid;
 
-if($result=mysqli_query($db_con,"DELETE FROM `login` where id=".$cntid))
+
+if($result=mysqli_query($db_con,"DELETE login,basic_faculty_info FROM login INNER JOIN basic_faculty_info on login.id = basic_faculty_info.id where login.id=".$cntid))
 {
     header("location: ../pages/webmaster_viewfaculty.php?message=SUCCESS IN DELETING&dep=");
 exit();
--- cms/DB_TRANS/deletefaculty.php
+++ cms/DB_TRANS/deletefaculty.php
@@ -3,7 +3,7 @@ $cntid=$_GET['id'];
 require_once('../DB_TRANS/db_con.php');
 $query="DELETE  FROM `staff` where id=".$cntid;
 
-if($result=mysqli_query($db_con,"SELECT * FROM `login` where id=".$cntid))
+if($result=mysqli_query($db_con,"DELETE login,basic_faculty_info FROM login INNER JOIN basic_faculty_info on login.id = basic_faculty_info.id where login.id=".$cntid))
 {
     header("location: ../pages/webmaster_editfaculty.php?message=SUCCESS IN DELETING ");
     exit();
